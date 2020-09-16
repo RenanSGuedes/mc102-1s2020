88f6f6886dbe091e252ed1741dfbab74dc2def9c
RenanSGuedes <renanguedes.br@gmail.com>
2020-09-15 23:02:22 -0300

    lab03 images

--- labs/lab03/lab03/lab03.py
+++ labs/lab03/lab03/lab03.py
@@ -3,13 +3,9 @@ with_fever = input()
 difficulty_breathing = input()
 with_covid = "True"
 
-def verify(a, b, c):
-  if a == "True" and b == "True" and c == "True":
-    return with_covid
-  else:
-    return with_covid == "False"
+verify = with_cough == "True" and with_fever == "True" and difficulty_breathing == "True" 
 
 print('Tosse:', with_cough == "True")
 print('Febre:', with_fever == "True")
 print('Dificuldade para respirar:', difficulty_breathing == "True")
-print('Provavelmente eh COVID-19:', verify(with_cough, with_fever, difficulty_breathing)) 
+print('Provavelmente eh COVID-19:', verify) 
